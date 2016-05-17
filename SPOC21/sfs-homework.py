ad1d4e66956de51280ee06340beee40d3a828822
ClarkChen <Clarkclk@users.noreply.github.com>
2016-05-17 20:02:59 +0800

    Update sfs-homework.py

--- SPOC21/sfs-homework.py
+++ SPOC21/sfs-homework.py
@@ -163,13 +163,13 @@ class inode:
     def free(self):
         self.ftype = 'free'
         self.addr  = -1
-        
+
 
 class fs:
     def __init__(self, numInodes, numData):
         self.numInodes = numInodes
         self.numData   = numData
-        
+
         self.ibitmap = bitmap(self.numInodes)
         self.inodes  = []
         for i in range(self.numInodes):
@@ -179,7 +179,7 @@ class fs:
         self.data    = []
         for i in range(self.numData):
             self.data.append(block('free'))
-    
+
         # root inode
         self.ROOT = 0
 
@@ -229,7 +229,7 @@ class fs:
     def dataFree(self, bnum):
         self.dbitmap.free(bnum)
         self.data[bnum].free()
-        
+
     def getParent(self, name):
         tmp = name.split('/')
         if len(tmp) == 2:
@@ -247,8 +247,21 @@ class fs:
 
     # YOUR CODE, 2012011304
         # IF inode.refcnt ==1, THEN free data blocks first, then free inode, ELSE dec indoe.refcnt
+        inode = self.inodes[inum]
+        if inode.refCnt == 1:
+            addr = inode.getAddr()
+            if addr != -1:
+                self.dataFree(addr)
+            self.inodeFree(inum)
+        else:
+            inode.decRefCnt()
+
         # remove from parent directory: delete from parent inum, delete from parent addr
-        _INODE = self.inodes[inum];
+        parent = self.getParent(tfile)
+        inum = self.nameToInum[parent]
+        self.data[self.inodes[inum].getAddr()].delDirEntry(tfile)
+        self.inodes[inum].decRefCnt()
+
     # DONE
 
         # finally, remove from files list
@@ -256,26 +269,65 @@ class fs:
         return 0
 
     def createLink(self, target, newfile, parent):
-    # YOUR CODE, YOUR ID
+    # YOUR CODE, 2012011304
         # find info about parent
+        pnum = self.nameToInum[parent]
+        paddr = self.inode[pnum].getAddr()
+
         # is there room in the parent directory?
+        if self.data[paddr].getFreeEntries() == 0:
+            return -1
+
         # if the newfile was already in parent dir?
+        if self.data[paddr].dirEntryExists(newfile):
+            return -1
+
         # now, find inumber of target
+        tinum = self.nameToInum[target]
+        self.inodes[tinum].incRefCnt()
+
         # inc parent ref count
-        # now add to directory
+        self.inodes[pnum].incRefCnt()
+
+         # now add to directory
+        self.data[paddr].addDirEntry(newfile, tinum)
+
     # DONE
         return tinum
 
     def createFile(self, parent, newfile, ftype):
     # YOUR CODE, YOUR ID
         # find info about parent
+        pnum = self.nameToInum[parent]
+        paddr = self.inodes[pnum].getAddr()
+
         # is there room in the parent directory?
+        if self.data[paddr].getFreeEntries() <= 0:
+            return -1
+
         # have to make sure file name is unique
+        if self.data[paddr].dirEntryExists(newfile):
+            return -1
+
         # find free inode
+        inum = self.inodeAlloc()
+
         # if a directory, have to allocate directory block for basic (., ..) info
+        if ftype == 'd':
+            iaddr = self.dataAlloc()
+            self.data[iaddr].setType('d')
+            self.data[iaddr].addDirEntry('.', inum)
+            self.data[iaddr].addDirEntry('..', pnum)
+
         # now ok to init inode properly
+        self.inodes[inum].setAll('d', iaddr, 2)
+
         # inc parent ref count
+        self.inodes[pnum].incRefCnt()
+
         # and add to directory of parent
+        self.data[paddr].addDirEntry(newfile, inum)
+
     # DONE
         return inum
 
@@ -284,16 +336,26 @@ class fs:
         curSize = self.inodes[inum].getSize()
         dprint('writeFile: inum:%d cursize:%d refcnt:%d' % (inum, curSize, self.inodes[inum].getRefCnt()))
 
-    # YOUR CODE, YOUR ID
+    # YOUR CODE, 2012011304
         # file is full?
+        if curSize == 1:
+            return -1
+
         # no data blocks left
+        addr = self.dataAlloc()
+        if addr == -1:
+            return -1
+
         # write file data
+        self.inodes[inum].setAddr(addr)
+        self.data[addr].setType('f')
+        self.data[addr].addData(data)
     # DONE
 
         if printOps:
             print 'fd=open("%s", O_WRONLY|O_APPEND); write(fd, buf, BLOCKSIZE); close(fd);' % tfile
         return 0
-            
+
     def doDelete(self):
         dprint('doDelete')
         if len(self.files) == 0:
@@ -327,7 +389,7 @@ class fs:
                 print 'link("%s", "%s");' % (target, fullName)
             return 0
         return -1
-    
+
     def doCreate(self, ftype):
         dprint('doCreate')
         parent = self.dirs[int(random.random() * len(self.dirs))]
@@ -378,7 +440,7 @@ class fs:
         print ''
         self.dump()
         print ''
-        
+
         for i in range(numRequests):
             if printOps == False:
                 print 'Which operation took place?'
@@ -424,8 +486,8 @@ class fs:
 parser = OptionParser()
 
 parser.add_option('-s', '--seed',        default=0,     help='the random seed',                      action='store', type='int', dest='seed')
-parser.add_option('-i', '--numInodes',   default=8,     help='number of inodes in file system',      action='store', type='int', dest='numInodes') 
-parser.add_option('-d', '--numData',     default=8,     help='number of data blocks in file system', action='store', type='int', dest='numData') 
+parser.add_option('-i', '--numInodes',   default=8,     help='number of inodes in file system',      action='store', type='int', dest='numInodes')
+parser.add_option('-d', '--numData',     default=8,     help='number of data blocks in file system', action='store', type='int', dest='numData')
 parser.add_option('-n', '--numRequests', default=10,    help='number of requests to simulate',       action='store', type='int', dest='numRequests')
 parser.add_option('-r', '--reverse',     default=False, help='instead of printing state, print ops', action='store_true',        dest='reverse')
 parser.add_option('-p', '--printFinal',  default=False, help='print the final set of files/dirs',    action='store_true',        dest='printFinal')
